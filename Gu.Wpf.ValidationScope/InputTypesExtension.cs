66ec4747857e4d46c74fcf19ae9816e9cca8609b
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-07 21:19:29 +0100

    Generate constructors for InputTypesExtension

--- Gu.Wpf.ValidationScope/InputTypesExtension.cs
+++ Gu.Wpf.ValidationScope/InputTypesExtension.cs
@@ -1,29 +1,12 @@
 namespace Gu.Wpf.ValidationScope
 {
     using System;
-    using System.Collections.Generic;
     using System.Windows.Markup;
 
     [MarkupExtensionReturnType(typeof(InputTypeCollection))]
-    public class InputTypesExtension : MarkupExtension
+    public partial class InputTypesExtension : MarkupExtension
     {
         public InputTypesExtension()
-        {
-        }
-
-        public InputTypesExtension(Type t1)
-            : this(new[] { t1 })
-        {
-        }
-
-        public InputTypesExtension(Type t1, Type t2)
-            : this(new[] { t1, t2 })
-        {
-        }
-
-
-        public InputTypesExtension(Type t1, Type t2, Type t3)
-            : this(new[] { t1, t2, t3 })
         {
         }
 
---
+++ Gu.Wpf.ValidationScope/InputTypesExtensionCtors.generated.cs
@@ -0,0 +1,99 @@
+﻿namespace Gu.Wpf.ValidationScope
+{
+    using System;
+	using System.Collections.Generic;
+
+    public partial class InputTypesExtension
+    {
+        public InputTypesExtension(Type t1)
+            : this(new[] { t1 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1)
+        //     : this(new[] { t1 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2)
+            : this(new[] { t1, t2 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2)
+        //     : this(new[] { t1, t2 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3)
+            : this(new[] { t1, t2, t3 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3)
+        //     : this(new[] { t1, t2, t3 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4)
+            : this(new[] { t1, t2, t3, t4 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4)
+        //     : this(new[] { t1, t2, t3, t4 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4, Type t5)
+            : this(new[] { t1, t2, t3, t4, t5 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4, Type t5)
+        //     : this(new[] { t1, t2, t3, t4, t5 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6)
+            : this(new[] { t1, t2, t3, t4, t5, t6 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6)
+        //     : this(new[] { t1, t2, t3, t4, t5, t6 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7)
+            : this(new[] { t1, t2, t3, t4, t5, t6, t7 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7)
+        //     : this(new[] { t1, t2, t3, t4, t5, t6, t7 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7, Type t8)
+            : this(new[] { t1, t2, t3, t4, t5, t6, t7, t8 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7, Type t8)
+        //     : this(new[] { t1, t2, t3, t4, t5, t6, t7, t8 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+        public InputTypesExtension(Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7, Type t8, Type t9)
+            : this(new[] { t1, t2, t3, t4, t5, t6, t7, t8, t9 })
+        {
+        }   
+
+		// public InputTypesExtension(IEnumerable<Type> types, Type t1, Type t2, Type t3, Type t4, Type t5, Type t6, Type t7, Type t8, Type t9)
+        //     : this(new[] { t1, t2, t3, t4, t5, t6, t7, t8, t9 })
+        // {
+		//    this.Types.AddRange(types);
+        // }   
+    }
+}
