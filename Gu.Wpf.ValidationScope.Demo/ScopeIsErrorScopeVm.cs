1d29e87ce7642337acc44f38c81ace3abc847d45
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-10 21:56:28 +0100

    Scope is ErrorNode.
    - Tried to repro an edge case but there was no bug.

---
+++ Gu.Wpf.ValidationScope.Demo/ScopeIsErrorScopeVm.cs
@@ -0,0 +1,58 @@
+namespace Gu.Wpf.ValidationScope.Demo
+{
+    using System;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+
+    using JetBrains.Annotations;
+
+    public class ScopeIsErrorScopeVm : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        private int intValue;
+        private bool hasErrors;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public bool HasErrors
+        {
+            get { return this.hasErrors; }
+            set
+            {
+                if (value == this.hasErrors) return;
+                this.hasErrors = value;
+                this.OnPropertyChanged();
+                this.OnErrorsChanged();
+            }
+        }
+
+        public int IntValue
+        {
+            get { return this.intValue; }
+            set
+            {
+                if (value == this.intValue) return;
+                this.intValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            return this.hasErrors ? new[] { "INotifyDataErrorInfo error" } : Enumerable.Empty<string>();
+        }
+
+        protected virtual void OnErrorsChanged([CallerMemberName] string propertyName = null)
+        {
+            this.ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+    }
+}
