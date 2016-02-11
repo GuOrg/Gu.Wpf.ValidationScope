61ac8083f398f17999c42c418d10b03aaf9b0fce
Johan Larsson <johan_gavle1@hotmail.com>
2016-02-11 14:17:34 +0100

    SandboxView

---
+++ Gu.Wpf.ValidationScope.Demo/SandboxView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.ValidationScope.Demo
+{
+    using System.Windows.Controls;
+
+    public partial class SandboxView : UserControl
+    {
+        public SandboxView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gu.Wpf.ValidationScope.Demo/SandboxVm.cs
@@ -0,0 +1,74 @@
+﻿namespace Gu.Wpf.ValidationScope.Demo
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using JetBrains.Annotations;
+
+    public class SandboxVm : INotifyPropertyChanged
+    {
+        private int value1;
+        private int value2;
+
+        private int value3;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Value1
+        {
+            get
+            {
+                return this.value1;
+            }
+            set
+            {
+                if (value == this.value1)
+                {
+                    return;
+                }
+                this.value1 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public int Value2
+        {
+            get
+            {
+                return this.value2;
+            }
+            set
+            {
+                if (value == this.value2)
+                {
+                    return;
+                }
+                this.value2 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public int Value3
+        {
+            get
+            {
+                return this.value3;
+            }
+            set
+            {
+                if (value == this.value3)
+                {
+                    return;
+                }
+                this.value3 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
