97d15e6fdcda7911a4de62d1be99a7369230a4fd
Johan Larsson <johan_gavle1@hotmail.com>
2016-09-07 19:03:29 +0200

    Fix DataGridWindowTests

--- Gu.Wpf.ValidationScope.Ui.Tests/DataGridWindowTests.cs
+++ Gu.Wpf.ValidationScope.Ui.Tests/DataGridWindowTests.cs
@@ -1,5 +1,7 @@
 namespace Gu.Wpf.ValidationScope.Ui.Tests
 {
+    using System.Collections.Generic;
+
     using Gu.Wpf.ValidationScope.Demo;
     using NUnit.Framework;
 
@@ -10,23 +12,63 @@ namespace Gu.Wpf.ValidationScope.Ui.Tests
     {
         protected override string WindowName { get; } = "DataGridWindow";
 
+
+        public GroupBox Scope => this.Window.GetByText<GroupBox>("Scope");
+
+        public IReadOnlyList<string> ScopeErrors => this.Scope.GetErrors();
+
+        public string ScopeHasError => this.Scope.Get<Label>("HasErrorTextBlock").Text;
+
+        public GroupBox Node => this.Window.GetByText<GroupBox>("Node");
+
+        public string ChildCount => this.Node.Get<Label>("ChildCountTextBlock").Text;
+
+        public IReadOnlyList<string> NodeErrors => this.Node.GetErrors();
+
+        public string NodeHasError => this.Node.Get<Label>("HasErrorTextBlock").Text;
+
+        public IReadOnlyList<string> NodeChildren => this.Node.GetChildren();
+
+        public string NodeType => this.Node.Get<Label>("NodeTypeTextBlock").Text;
+
         [Test]
-        public void Updates()
+        public void AddThenRemoveError()
         {
-            CollectionAssert.IsEmpty(this.Window.GetErrors());
+            Assert.AreEqual("HasError: False", this.ScopeHasError);
+            CollectionAssert.IsEmpty(this.ScopeErrors);
+
+            Assert.AreEqual("Children: 0", this.ChildCount);
+            Assert.AreEqual("HasError: False", this.NodeHasError);
+            CollectionAssert.IsEmpty(this.NodeErrors);
+            Assert.AreEqual("Gu.Wpf.ValidationScope.ValidNode", this.NodeType);
 
             var dataGrid = this.Window.Get<ListView>(AutomationIDs.DataGrid);
             var cell = dataGrid.Rows[0].Cells[0];
             cell.Click();
             cell.Enter("a");
             this.Window.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.TAB);
-            CollectionAssert.AreEqual(new[] { "Value 'a' could not be converted." }, this.Window.GetErrors());
+            var expectedErrors = new[] { "Value 'a' could not be converted." };
+            Assert.AreEqual("HasError: True", this.ScopeHasError);
+            CollectionAssert.AreEqual(expectedErrors, this.ScopeErrors);
+
+            Assert.AreEqual("Children: 1", this.ChildCount);
+            Assert.AreEqual("HasError: True", this.NodeHasError);
+            CollectionAssert.AreEqual(expectedErrors, this.NodeErrors);
+            CollectionAssert.AreEqual(new[] { "System.Windows.Controls.DataGrid Items.Count:3" }, this.NodeChildren);
+            Assert.AreEqual("Gu.Wpf.ValidationScope.ScopeNode", this.NodeType);
 
             this.Window.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.SHIFT, KeyboardInput.SpecialKeys.TAB);
             this.Window.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.BACKSPACE);
             cell.Enter("2");
             this.Window.Keyboard.PressSpecialKey(KeyboardInput.SpecialKeys.TAB);
-            CollectionAssert.IsEmpty(this.Window.GetErrors());
+
+            Assert.AreEqual("HasError: False", this.ScopeHasError);
+            CollectionAssert.IsEmpty(this.ScopeErrors);
+
+            Assert.AreEqual("Children: 0", this.ChildCount);
+            Assert.AreEqual("HasError: False", this.NodeHasError);
+            CollectionAssert.IsEmpty(this.NodeErrors);
+            Assert.AreEqual("Gu.Wpf.ValidationScope.ValidNode", this.NodeType);
         }
     }
 }
