22709253a492d461ac8207dc8ef2c2ce71bae35e
JohanLarsson <johan_gavle1@hotmail.com>
2017-09-09 09:09:34 +0200

    Refactor EventsOrderWindowTests

--- Gu.Wpf.ValidationScope.Ui.Tests/EventsOrderWindowTests.cs
+++ Gu.Wpf.ValidationScope.Ui.Tests/EventsOrderWindowTests.cs
@@ -2,153 +2,169 @@ namespace Gu.Wpf.ValidationScope.Ui.Tests
 {
     using System.Collections.Generic;
     using System.Linq;
+    using Gu.Wpf.UiAutomation;
     using NUnit.Framework;
 
-    public class EventsOrderWindowTests : WindowTests
+    public class EventsOrderWindowTests
     {
-        protected override string WindowName { get; } = "EventsOrderWindow";
+        private static readonly string WindowName = "EventsOrderWindow";
 
         [Test]
         public void Validation()
         {
-            // this is used as reference
-            var groupBox = this.Window.FindGroupBox("Validation events");
-            var expected = new List<string> { "HasError: False", "Empty" };
-            var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                // this is used as reference
+                var groupBox = window.FindGroupBox("Validation events");
+                var expected = new List<string> { "HasError: False", "Empty" };
+                var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            var textBox = this.Window.FindTextBox("ValidationTextBox");
-            textBox.Text = "a";
-            expected.AddRange(
-                new[]
-                    {
+                var textBox = window.FindTextBox("ValidationTextBox");
+                textBox.Text = "a";
+                expected.AddRange(
+                    new[]
+                        {
                         "ValidationError: Value 'a' could not be converted.",
                         "HasError: True",
                         "Action: Added Error: Value 'a' could not be converted. Source: ValidationTextBox OriginalSource: ValidationTextBox"
-                    });
+                        });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            textBox.Text = "1";
-            expected.AddRange(new[]
-                           {
+                textBox.Text = "1";
+                expected.AddRange(new[]
+                               {
                                "HasError: False",
                                "Empty",
                                "Action: Removed Error: Value 'a' could not be converted. Source: ValidationTextBox OriginalSource: ValidationTextBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            }
         }
 
         [Test]
         public void ScopeTextBox()
         {
-            var groupBox = this.Window.FindGroupBox("Scope textbox events");
-            var expected = new List<string> { "HasError: False", "Empty" };
-            var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("Scope textbox events");
+                var expected = new List<string> { "HasError: False", "Empty" };
+                var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            var textBox = this.Window.FindTextBox("ScopeTextBox");
-            textBox.Text = "a";
-            expected.AddRange(new[]
-                               {
+                var textBox = window.FindTextBox("ScopeTextBox");
+                textBox.Text = "a";
+                expected.AddRange(new[]
+                                   {
                                    "ValidationError: Value 'a' could not be converted.",
                                    "HasError: True",
                                    "Action: Added Error: Value 'a' could not be converted. Source: ScopeTextBox OriginalSource: ScopeTextBox"
                                });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            textBox.Text = "1";
-            expected.AddRange(new[]
-                           {
+                textBox.Text = "1";
+                expected.AddRange(new[]
+                               {
                                "HasError: False",
                                "Empty",
                                "Action: Removed Error: Value 'a' could not be converted. Source: ScopeTextBox OriginalSource: ScopeTextBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            }
         }
 
         [Test]
         public void ScopeGroupBoxOneError()
         {
-            this.RestartApplication();
-            var groupBox = this.Window.FindGroupBox("Scope events");
-            var expected = new List<string> { "HasError: False", "Empty" };
-            var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("Scope events");
+                var expected = new List<string> { "HasError: False", "Empty" };
+                var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            var textBox1 = this.Window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox1");
-            textBox1.Text = "a";
-            expected.AddRange(new[]
-                           {
+                var textBox1 = window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox1");
+                textBox1.Text = "a";
+                expected.AddRange(new[]
+                               {
                                "ValidationError: Value 'a' could not be converted.",
                                "HasError: True",
                                "Action: Added Error: Value 'a' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            textBox1.Text = "1";
-            expected.AddRange(new[]
-                           {
+                textBox1.Text = "1";
+                expected.AddRange(new[]
+                               {
                                "HasError: False",
                                "Empty",
                                "Action: Removed Error: Value 'a' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            }
         }
 
         [Test]
         public void ScopeGroupBoxTwoErrors()
         {
-            this.RestartApplication();
-            var groupBox = this.Window.FindGroupBox("Scope events");
-            var expected = new List<string> { "HasError: False", "Empty" };
-            var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            using (var app = Application.Launch(Info.ExeFileName, WindowName))
+            {
+                var window = app.MainWindow;
+                var groupBox = window.FindGroupBox("Scope events");
+                var expected = new List<string> { "HasError: False", "Empty" };
+                var actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            var textBox1 = this.Window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox1");
-            textBox1.Text = "a";
-            expected.AddRange(new[]
-                           {
+                var textBox1 = window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox1");
+                textBox1.Text = "a";
+                expected.AddRange(new[]
+                               {
                                "ValidationError: Value 'a' could not be converted.",
                                "HasError: True",
                                "Action: Added Error: Value 'a' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            var textBox2 = this.Window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox2");
-            textBox2.Text = "b";
-            expected.AddRange(new[]
-                           {
+                var textBox2 = window.FindGroupBox("ScopeGroupBox").FindTextBox("ScopeTextBox2");
+                textBox2.Text = "b";
+                expected.AddRange(new[]
+                               {
                                "Action: Added Error: Value 'b' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox"
                            });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
 
-            textBox1.Text = "1";
-            expected.AddRange(new[]
-            {
-                "Action: Removed Error: Value 'a' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox",
-                "HasError: False",
-                "Empty",
-                "Action: Removed Error: Value 'b' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox",
-            });
+                textBox1.Text = "1";
+                expected.AddRange(
+                    new[]
+                    {
+                        "Action: Removed Error: Value 'a' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox",
+                        "HasError: False",
+                        "Empty",
+                        "Action: Removed Error: Value 'b' could not be converted. Source: ScopeGroupBox OriginalSource: ScopeGroupBox",
+                    });
 
-            actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
-            CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+                actual = groupBox.FindTextBlocks("Event").Select(x => x.Text).ToArray();
+                CollectionAssert.AreEqual(expected, actual, $"Actual: {string.Join(", ", actual.Select(x => "\"" + x + "\""))}");
+            }
         }
     }
 }
