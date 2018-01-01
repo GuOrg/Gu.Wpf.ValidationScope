8b39e049beafd274e8abbe07cba782e644d6b596
Johan Larsson <johan_gavle1@hotmail.com>
2018-01-01 11:36:35 +0100

    Fix tests.

---
+++ Gu.Wpf.ValidationScope.Ui.Tests/Helpers/UiElementExt.cs
@@ -0,0 +1,51 @@
+namespace Gu.Wpf.ValidationScope.Ui.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Automation;
+    using Gu.Wpf.UiAutomation;
+    using Condition = Gu.Wpf.UiAutomation.Condition;
+
+    public static class UiElementExt
+    {
+        public static IReadOnlyList<TextBlock> FindTextBlocks(this UiElement container, string automationId)
+        {
+            return container.FindAllDescendants(
+                                new AndCondition(
+                                    Condition.ByClassName("TextBlock"),
+                                    new OrCondition(
+                                        Condition.ByAutomationId(automationId),
+                                        Condition.ByName(automationId))))
+                            .Select(x => x.AsTextBlock())
+                            .ToList();
+        }
+
+        public static IReadOnlyList<TextBlock> FindTextBlocks(this UiElement container)
+        {
+            return container.FindAllDescendants(Condition.ByClassName("TextBlock"))
+                            .Select(x => x.AsTextBlock())
+                            .ToList();
+        }
+
+        public static IReadOnlyList<TextBox> FindTextBoxes(this UiElement container)
+        {
+            return container.FindAllDescendants(Condition.ByClassName("TextBox"))
+                            .Select(x => x.AsTextBox())
+                            .ToList();
+        }
+
+        public static IReadOnlyList<string> GetErrors(this UiElement container)
+        {
+            return container.FindTextBlocks("ErrorTextBlock")
+                            .Select(x => x.Text)
+                            .ToList();
+        }
+
+        public static IReadOnlyList<string> GetChildren(this UiElement container)
+        {
+            return container.FindTextBlocks("ChildTextBlock")
+                            .Select(x => x.Text)
+                            .ToList();
+        }
+    }
+}
