2da0950396ecd2782e3d991eb099e86146803695
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-03 19:46:03 +0100

    Amend.

--- Gu.Wpf.ValidationScope.Demo/DataGridScopeView.xaml.cs
+++ Gu.Wpf.ValidationScope.Demo/DataGridScopeView.xaml.cs
@@ -1,10 +1,7 @@
-﻿namespace Gu.Wpf.ValidationScope.Demo
+namespace Gu.Wpf.ValidationScope.Demo
 {
     using System.Windows.Controls;
 
-    /// <summary>
-    /// Interaction logic for DataGridScopeView.xaml.
-    /// </summary>
     public partial class DataGridScopeView : UserControl
     {
         public DataGridScopeView()
