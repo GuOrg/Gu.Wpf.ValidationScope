4fe0192a47d23729f65ba26b4a2e6ae209d8898e
JohanLarsson <johan_gavle1@hotmail.com>
2020-01-22 15:08:11 +0100

    Add gu prefix.
    Pretty sure there can be many.

--- Gu.Wpf.ValidationScope/AssemblyAttributes.cs
+++ Gu.Wpf.ValidationScope/AssemblyAttributes.cs
@@ -5,6 +5,8 @@ using System.Windows.Markup;
 [assembly: InternalsVisibleTo("Gu.Wpf.ValidationScope.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010081C17142540667A1F595C54AD2B5916395C55177D9770A78B6610298EA5B0442D18B95AE9A789CF9D5C04A88B0E1BFE73E7FD2E033BDC37F4E7916376F4BD7E213BD792EBBB77DFEADF5F8D221289AA0EF2130CE8BDD33B1526F0EE2D6C1B472F956E2A3067769435880507D779468531A3C97270628E44FC15727B0BC915BC7", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Wpf.ValidationScope.Demo, PublicKey=002400000480000094000000060200000024000052534131000400000100010081C17142540667A1F595C54AD2B5916395C55177D9770A78B6610298EA5B0442D18B95AE9A789CF9D5C04A88B0E1BFE73E7FD2E033BDC37F4E7916376F4BD7E213BD792EBBB77DFEADF5F8D221289AA0EF2130CE8BDD33B1526F0EE2D6C1B472F956E2A3067769435880507D779468531A3C97270628E44FC15727B0BC915BC7", AllInternalsVisible = true)]
 
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.None)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.ValidationScope", "Gu.Wpf.ValidationScope")]
 [assembly: XmlnsPrefix("https://github.com/JohanLarsson/Gu.Wpf.ValidationScope", "validation")]
+[assembly: XmlnsDefinition("https://github.com/GuOrg/Gu.Wpf.ValidationScope", "Gu.Wpf.ValidationScope")]
+[assembly: XmlnsPrefix("https://github.com/GuOrg/Gu.Wpf.ValidationScope", "gu")]
