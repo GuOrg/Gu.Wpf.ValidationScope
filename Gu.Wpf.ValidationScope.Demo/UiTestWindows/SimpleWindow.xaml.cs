cc8591bb3cd7046a9b4bf703fdc03a081ea4df9d
JohanLarsson <johan_gavle1@hotmail.com>
2020-07-13 14:30:53 +0200

    Fix warnings.

--- Gu.Wpf.ValidationScope.Demo/UiTestWindows/SimpleWindow.xaml.cs
+++ Gu.Wpf.ValidationScope.Demo/UiTestWindows/SimpleWindow.xaml.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace Gu.Wpf.ValidationScope.Demo.UiTestWindows
+﻿namespace Gu.Wpf.ValidationScope.Demo.UiTestWindows
 {
-    /// <summary>
-    /// Interaction logic for SimpleWindow.xaml
-    /// </summary>
+    using System.Windows;
+
     public partial class SimpleWindow : Window
     {
         public SimpleWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
