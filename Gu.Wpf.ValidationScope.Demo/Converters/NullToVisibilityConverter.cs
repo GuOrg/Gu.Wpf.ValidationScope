12c95da85280da31db4ee69dbfcb1397a0831aa7
JohanLarsson <johan_gavle1@hotmail.com>
2020-08-08 18:17:47 +0200

    NullToVisibilityConverter

---
+++ Gu.Wpf.ValidationScope.Demo/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Gu.Wpf.ValidationScope.Demo
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public sealed class NullToVisibilityConverter : IValueConverter
+    {
+        public static readonly NullToVisibilityConverter CollapsedWhenNull = new NullToVisibilityConverter(Visibility.Collapsed);
+        private readonly Visibility whenNull;
+
+        private NullToVisibilityConverter(Visibility whenNull)
+        {
+            this.whenNull = whenNull;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is null ? this.whenNull: Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
