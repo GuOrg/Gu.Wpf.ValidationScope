64a21501e024b3e079b17e5a96404208c9a9671d
JohanLarsson <johan_gavle1@hotmail.com>
2021-01-04 15:23:57 +0100

    [assembly: CLSCompliant(false)]

--- Gu.Wpf.ValidationScope.Demo/AssemblyAttributes.cs
+++ Gu.Wpf.ValidationScope.Demo/AssemblyAttributes.cs
@@ -1,3 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+
+[assembly: CLSCompliant(false)]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
